353cd6c3de2c58bdc641e0d7767b790bdc8c4bd9
Raikashi <70040264+RAIKASHI106@users.noreply.github.com>
2025-04-12 19:33:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -118,8 +118,17 @@ registerSlashCommand(
         const tags = value.trim().split(/\s+/).join(' ');
         const url = `https://danbooru.donmai.us/posts?tags=${encodeURIComponent(tags)}`;
         sendSystemMessage('generic', ` Here is your image.....`);
+
+        const response = await fetch(`https://danbooru.donmai.us/posts.json?tags=${encodeURIComponent(tags)}&limit=20`);
+
+        const data = await response.json();
+        const randomPost = data[Math.floor(Math.random() * data.length)];
+        const imageUrl = randomPost?.file_url;
+        
         window.open(url, '_blank');
         return `🔍 Opened Danbooru with tags: ${tags}`;
+
+        window.open(imageUrl, '_blank');
     },
     [],
     'Open Danbooru with tags. Example: /ctx-help hinata bikini',
