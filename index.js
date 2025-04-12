fce3d25e846203f7c0575b89b2c687e555011221
Raikashi <70040264+RAIKASHI106@users.noreply.github.com>
2025-04-12 17:01:22 -0400

    Update index.js adds in the first iteration of the / command

--- index.js
+++ index.js
@@ -1,63 +1,56 @@
-// The main script for the extension
-// The following are examples of some basic extension functionality
-
-//You'll likely need to import extension_settings, getContext, and loadExtensionSettings from extensions.js
-import { extension_settings, getContext, loadExtensionSettings } from "../../../extensions.js";
-
-//You'll likely need to import some other functions from the main script
+import { extension_settings } from "../../../extensions.js";
 import { saveSettingsDebounced } from "../../../../script.js";
 
-// Keep track of where your extension is located, name should match repo name
-const extensionName = "st-extension-example";
+const extensionName = "danbooru-autotagger";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
 const extensionSettings = extension_settings[extensionName];
 const defaultSettings = {};
 
-
- 
-// Loads the extension settings if they exist, otherwise initializes them to the defaults.
+// Settings init
 async function loadSettings() {
-  //Create the settings if they don't exist
   extension_settings[extensionName] = extension_settings[extensionName] || {};
   if (Object.keys(extension_settings[extensionName]).length === 0) {
     Object.assign(extension_settings[extensionName], defaultSettings);
   }
-
-  // Updating settings in the UI
-  $("#example_setting").prop("checked", extension_settings[extensionName].example_setting).trigger("input");
 }
 
-// This function is called when the extension settings are changed in the UI
+// Command logic
+function handleDanbooruCommand(args) {
+  const tags = args.join("+");
+  const url = `https://danbooru.donmai.us/posts?tags=${encodeURIComponent(tags)}`;
+  window.open(url, "_blank");
+}
+
+// Register the slash command
+function registerCommands() {
+  if (window.SillyTavern && window.SillyTavern.Commands) {
+    SillyTavern.Commands.register("danbooru", {
+      description: "Search Danbooru with tags",
+      usage: "/danbooru tag1 tag2",
+      run: (args) => handleDanbooruCommand(args),
+    });
+  }
+}
+
+// Extension entry point
+jQuery(async () => {
+  const settingsHtml = await $.get(`${extensionFolderPath}/settings.html`);
+  $("#extensions_settings2").append(settingsHtml);
+
+  $("#my_button").on("click", onButtonClick);
+  $("#example_setting").on("input", onExampleInput);
+
+  loadSettings();
+  registerCommands();
+});
+
+// Optional button logic from template
+function onButtonClick() {
+  toastr.info("Danbooru Autotagger is active!");
+}
+
 function onExampleInput(event) {
   const value = Boolean($(event.target).prop("checked"));
   extension_settings[extensionName].example_setting = value;
   saveSettingsDebounced();
 }
-
-// This function is called when the button is clicked
-function onButtonClick() {
-  // You can do whatever you want here
-  // Let's make a popup appear with the checked setting
-  toastr.info(
-    `The checkbox is ${extension_settings[extensionName].example_setting ? "checked" : "not checked"}`,
-    "A popup appeared because you clicked the button!"
-  );
-}
-
-// This function is called when the extension is loaded
-jQuery(async () => {
-  // This is an example of loading HTML from a file
-  const settingsHtml = await $.get(`${extensionFolderPath}/example.html`);
-
-  // Append settingsHtml to extensions_settings
-  // extension_settings and extensions_settings2 are the left and right columns of the settings menu
-  // Left should be extensions that deal with system functions and right should be visual/UI related 
-  $("#extensions_settings").append(settingsHtml);
-
-  // These are examples of listening for events
-  $("#my_button").on("click", onButtonClick);
-  $("#example_setting").on("input", onExampleInput);
-
-  // Load settings when starting things up (if you have any)
-  loadSettings();
-});
